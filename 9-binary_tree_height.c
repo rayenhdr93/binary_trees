b752ee5b85055e4ae9d21fea45e0e846096437c4
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-23 16:50:48 +0100

    fixing 9-

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,8 +6,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l;
-	size_t height_r;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
