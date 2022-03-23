32525c1b6ab8f31e8022fe18feb8229f698191c5
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-23 16:59:46 +0100

    fixing 10-

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,6 +8,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
+	if (tree != NULL)
+	{
 	depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	}
 	return (depth);
 }
